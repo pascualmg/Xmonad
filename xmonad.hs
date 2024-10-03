1104babaf8d7f568659295925dee21ab78a949dd
pascualmg <pashkiter@gmail.com>
2024-10-03 10:52:41 +0200

    ewmh compatibility & gridselect

--- xmonad.hs
+++ xmonad.hs
@@ -10,6 +10,8 @@ import qualified XMonad.StackSet as W
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run (spawnPipe)
 import System.IO (hPutStrLn)
+import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
+import XMonad.Actions.GridSelect
 
 myStartupHook :: X ()
 myStartupHook = do
@@ -26,15 +28,17 @@ myDmenuCommand = "(flatpak list --app --columns=application | sed 's/^/flatpak r
 
 myScratchPads = [
     NS "terminal" "alacritty --class scratchpad -e byobu" (className =? "scratchpad")
-        (customFloating $ W.RationalRect 0.1 0.1 0.9 0.9),
+        (customFloating $ W.RationalRect 0.1 0.1 0.8 0.8),
     NS "doom" "doom run --class=scratchpad-notes" (className =? "scratchpad-notes")
-        (customFloating $ W.RationalRect 0.15 0.1 0.7 0.75)
+        (customFloating $ W.RationalRect 0.15 0.1 0.7 0.75),
+    NS "toolbox" "~/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox --class=scratchpad-jetbrains" (className =? "scratchpad-jetbrains")
+        (customFloating $ W.RationalRect 0.1 0.1 0.8 0.8)
     ]
 
 main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar"  -- Inicia xmobar
-    xmonad $ docks $ def
+    xmonad $ ewmh $ ewmhFullscreen $ docks $ def
         { modMask = mod4Mask  -- Usa la tecla Windows como modificador
         , terminal = "alacritty"
         , borderWidth = 1
@@ -43,7 +47,8 @@ main = do
         , startupHook = myStartupHook
         , manageHook = namedScratchpadManageHook myScratchPads <+> manageHook def
         , layoutHook = avoidStruts $ layoutHook def  -- Evita que las ventanas cubran xmobar
-        , logHook = dynamicLogWithPP $ xmobarPP
+        , handleEventHook = handleEventHook def
+        , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppCurrent = xmobarColor "#c3e88d" "" . wrap "[" "]"
             , ppVisible = xmobarColor "#c3e88d" ""
@@ -60,19 +65,24 @@ main = do
         , ("<XF86AudioLowerVolume>", spawn "amixer set Master 2%-")  -- Bajar volumen
         , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 2%+")  -- Subir volumen
         , ("<XF86AudioMute>", spawn "amixer set Master toggle")  -- Silenciar/Activar sonido
-        --tts
+        -- tts
         , ("M-t", spawn "xsel -o | python3 ~/.config/xmonad/my-scripts/tts.py")
         -- Hunde todas las ventanas flotantes
-        , ("M-s", sinkAll)
-        --ides
+        , ("M-S-s", sinkAll)
+        -- ides
         , ("M-S-e", spawn "/home/passh/.config/emacs/bin/doom run")
         , ("M-i", spawn "$(which idea)")
-        --keyboard es-us layout toggling izi
+        -- keyboard es-us layout toggling izi
         , ("M-ñ", spawn "~/.config/xmonad/my-scripts/toggle-keyboard-layout.sh")
         , ("M-;", spawn "~/.config/xmonad/my-scripts/toggle-keyboard-layout.sh")
-        --scratchpads
+        -- scratchpads
         , ("M-a", namedScratchpadAction myScratchPads "terminal")
         , ("M-e", namedScratchpadAction myScratchPads "doom")
+        , ("M-j", namedScratchpadAction myScratchPads "toolbox")
         -- Flameshot GUI
         , ("<Print>", spawn "flameshot gui")
+        -- Skippy-XD
+--        , ("M-s", spawn "skippy-xd --activate-window-picker")
+        , ("M-s", goToSelected def)
+
         ]
